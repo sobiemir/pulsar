b5cdb17431a7cca1fc61dc2359401274c6debe12
sobiemir <sobiemir@aculo.pl>
2019-11-16 18:27:17 +0100

    Add database settings to php file

--- server/application/bootstrap/settings.php
+++ server/application/bootstrap/settings.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $builder) {
-    $main_config = json_decode(file_get_contents('../environment/settings.json'));
-    $additional_config = json_decode(file_get_contents('../environment/settings.' . PULSAR_ENVIRONMENT . '.json'));
+    $database = require_once __DIR__ . '../settings/database.php';
 
     $builder->addDefinitions([
-        'settings' => array_merge($main_config, $additional_config)
+        'settings' => [
+            'database' => $database
+        ]
     ]);
 };
---
+++ server/application/settings/database.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '../../var/db.sqlite',
+    'memory' => false,
+    'user' => null,
+    'password' => null,
+    'prefix' => 'pulsar',
+    // 'schema' => null
+];
